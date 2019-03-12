675704a274333093ec3db933567444723739f4de
Cody Cornelison <codysmart@gmail.com>
2019-03-11 21:54:06 -0700

    Add inDev check for quick port switching.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express  = require("express");
 var app      = express();
 var port     = process.env.PORT || 8080;
 
+var inDev    = true;
+
+
 app.use(express.static("public"));
 
 app.set("views", "views");
@@ -10,6 +13,10 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
 	res.render("index");
 });
+
+if (inDev) {
+	port = 8080;
+}
 
 app.listen(port, function() {
 	console.log("port: " + port);
